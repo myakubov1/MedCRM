0c5b097bccd9f8d1c8038be3b2353bf17368fee9
Maksimilian Iakubov <myakubov1996@gmail.com>
2023-03-08 21:50:03 +0200

    Added login. Added getUsers.

--- backend/router.js
+++ backend/router.js
@@ -15,9 +15,10 @@ router.delete('/messages/:id', MessageController.delete);
 // users
 router.post('/registration', [
   check('username', 'Имя пользователя не может быть пустым').notEmpty(),
-  check('password', 'Пароль должен быть больше 4 и меньше 10 символов').isLength({ min: 4, max: 10 }),
+  check('password', 'Пароль должен быть больше 4 и меньше 20 символов').isLength({ min: 4, max: 20 }),
 ], AuthController.registration);
 router.post('/login', AuthController.login);
+router.post('/regadmin', AuthController.registrationAdmin);
 router.get('/users', AuthController.getUsers);
 
 export default router;
--- backend/users/AuthController.js
+++ backend/users/AuthController.js
@@ -3,15 +3,41 @@
 /* eslint-disable no-unused-vars */
 import bcrypt from 'bcryptjs';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import User from './User.js';
 import Role from './Role.js';
 
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, process.env.SECRET, { expiresIn: '24h' });
+};
 class AuthController {
+  async registrationAdmin(req, res) {
+    try {
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res.status(400).json({ message: 'User is already created' });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: 'ADMIN' });
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] });
+      await user.save();
+      return res.json({ message: 'User successfuly created' });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: 'Registration error' });
+    }
+  }
+
   async registration(req, res) {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(400).json({ message: 'Ошибка при регистрации', errors });
+        return res.status(400).json({ message: 'Registration error', errors });
       }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
@@ -22,7 +48,7 @@ class AuthController {
       const userRole = await Role.findOne({ value: 'USER' });
       const user = new User({ username, password: hashPassword, roles: [userRole.value] });
       await user.save();
-      return res.json({ message: 'User successfuly creted' });
+      return res.json({ message: 'User successfuly created' });
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: 'Registration error' });
@@ -31,7 +57,17 @@ class AuthController {
 
   async login(req, res) {
     try {
-
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ message: `User ${username} not found!` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: 'Login or password incorrect' });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (e) {
       console.log(e);
       res.status(500).json(e);
@@ -40,7 +76,8 @@ class AuthController {
 
   async getUsers(req, res) {
     try {
-
+      const users = await User.find();
+      return res.status(200).json(users);
     } catch (e) {
       res.status(500).json(e);
     }
